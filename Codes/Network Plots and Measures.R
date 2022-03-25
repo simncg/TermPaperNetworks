0b6d8d0f1df43e820e30a27fccf372d483a4674a
Deslava90 <79642529+Deslava90@users.noreply.github.com>
2022-03-25 16:22:33 +0100

    Update in script for network analysis

--- Codes/Network Plots and Measures.R
+++ Codes/Network Plots and Measures.R
@@ -1,5 +1,6 @@
+rm(list=ls())
 
-setwd("G:/Mi unidad/Classroom/21D009 Networks  Concepts and Algorithms 21-22 DS T2/Paper")
+setwd("/Users/deslava/Documents/GitHub/TermPaperNetworks")
 
 library(igraph)
 library(readxl)
@@ -24,13 +25,10 @@ node_coords<-df[,c("stop_id", "stop_lat", "stop_lon")] %>%
 node_bounds<-node_coords %>% 
   sp::SpatialPointsDataFrame(coords = .[,2:3],
                              data = .) %>%
-  sp::bbox() * 1.1
-
-
+  sp::bbox() * 1
 
 # Create network
 network <- graph_from_data_frame(d=df_network, directed=T) 
-
 
 # Coordinates of each point
 el_coords <-
@@ -40,11 +38,15 @@ el_coords <-
   full_join(node_coords, by = c("V2" = "stop_id")) %>%
   drop_na(V1, V2)
 
+bog_bb <- c(left = node_bounds[2,1]-0.01,
+            bottom = node_bounds[1,1]-0.01,
+            right = node_bounds[2,2]+0.01,
+            top = node_bounds[1,2]+0.01)
 
-# Same map as https://datosabiertos-transmilenio.hub.arcgis.com/datasets/5365d814bbdd4062a59234eea7d70db7/explore?location=4.661887%2C-74.108975%2C11.86
+bog_st <- get_map(location= bog_bb, maptype = "terrain", zoom=13)
 
 # Map with transportation network
-base_gg<-ggplot()+
+base_gg <- ggmap(bog_st) + 
   geom_point(data=node_coords,
              aes(x=long, y=lat), color="red")+
   geom_segment(data = el_coords, 
@@ -54,14 +56,12 @@ base_gg<-ggplot()+
                alpha=0.5)+
   theme_minimal()
 
-
 base_gg
 
 
-# Computing Manhattan distance between each station (node) 
-
+#----------Computing Manhattan distance between each station (node)------------#
 # Creating manhattan distance matrix 
-manhattan_distances<-as.matrix(dist(node_coords[,c(2,3)], method="manhattan"))
+manhattan_distances<-as.matrix(dist(node_coords[,c(2,3)], method="manhattan"))*111
 colnames(manhattan_distances)<-node_coords$stop_id
 rownames(manhattan_distances)<-node_coords$stop_id
 
@@ -69,43 +69,51 @@ rownames(manhattan_distances)<-node_coords$stop_id
 df_network$distance<-NA
 for(i in 1:nrow(df_network)){
 
-  # if(str_starts(as.character(df_network[i,1]), "0")){
-  #   x<-sub('.', '', df_network[i,1])
-  #   
-  # } else{
-    x<-as.character(df_network[i,1])
+  x<-as.character(df_network[i,1])
     
-  # }
-  # if(str_starts(as.character(df_network[i,2]), "0")){
-  #  y<-sub('.', '', df_network[i,2])
-    
-  #} else{
-    y<-as.character(df_network[i,2])
-  #}
-  
-  # print("New pair")
-  # print(x)
-  # print(y)
-  # print(paste("Row", i))
-  # 
-  
+  y<-as.character(df_network[i,2])
+ 
   df_network$distance[i]<-manhattan_distances[x,y]
 }
 
-
-# Now that we have the distance, let's define the network again
-network <- graph_from_data_frame(d=df_network, directed=T) 
-
+#--------------------------Topological Profile---------------------------------#
 # Average Degree
 degree_nodes<-degree(network, mode="all", loops = F, normalized = F)
 mean(degree_nodes)
 
-# Edge Betweeness
-edge_betweenness(network, weights= E(network)$distance)
+# Betweenness centrality - node
+btw <- betweenness(network, directed=T, weights=E(network)$distance)
+mean(btw)
 
-# Node Betweeness
-betw<-betweenness(network, weights= E(network)$distance)
+# Clustering
+transitivity(network, type="global")
 
-# Mean betweeness
-mean(betw)
+#Average Path Length
+path_length <- distances(network, mode = "all", weights = E(network)$distance)
+mean(path_length)
+
+
+#--------------------------Robustness measures---------------------------------#
+# Robustness Indicator
+
+# Robustness Metric
+
+# Critical Threshold
+
+# # Now that we have the distance, let's define the network again
+# network <- graph_from_data_frame(d=df_network, directed=T) 
+# 
+# # Average Degree
+# degree_nodes<-degree(network, mode="all", loops = F, normalized = F)
+# mean(degree_nodes)
+# 
+# # Edge Betweenness
+# edge_betweenness(network, weights= E(network)$distance)
+# 
+# # Node Betweenness
+# betw<-betweenness(network, weights= E(network)$distance)
+# 
+# # Mean betweeness
+# mean(betw)
+
 
