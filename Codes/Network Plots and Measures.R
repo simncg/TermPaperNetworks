7ac42a3fa425ed3c49b96df72a6e2dba5d46dbe8
simncg <48518938+simncg@users.noreply.github.com>
2022-03-25 05:37:30 +0100

    Update Networks and Measures script
    Update Networks and Measures script which had an error on the computation of the distances

--- Codes/Network Plots and Measures.R
+++ Codes/Network Plots and Measures.R
@@ -26,6 +26,12 @@ node_bounds<-node_coords %>%
                              data = .) %>%
   sp::bbox() * 1.1
 
+
+
+# Create network
+network <- graph_from_data_frame(d=df_network, directed=T) 
+
+
 # Coordinates of each point
 el_coords <-
   as_edgelist(network) %>%
@@ -34,6 +40,8 @@ el_coords <-
   full_join(node_coords, by = c("V2" = "stop_id")) %>%
   drop_na(V1, V2)
 
+
+# Same map as https://datosabiertos-transmilenio.hub.arcgis.com/datasets/5365d814bbdd4062a59234eea7d70db7/explore?location=4.661887%2C-74.108975%2C11.86
 
 # Map with transportation network
 base_gg<-ggplot()+
@@ -50,41 +58,54 @@ base_gg<-ggplot()+
 base_gg
 
 
-
-
-
 # Computing Manhattan distance between each station (node) 
 
 # Creating manhattan distance matrix 
 manhattan_distances<-as.matrix(dist(node_coords[,c(2,3)], method="manhattan"))
-colnames(manhattan_distances)<-as.numeric(node_coords$stop_id)
-rownames(manhattan_distances)<-as.numeric(node_coords$stop_id)
+colnames(manhattan_distances)<-node_coords$stop_id
+rownames(manhattan_distances)<-node_coords$stop_id
 
 # Adding distances between any adge
 df_network$distance<-NA
 for(i in 1:nrow(df_network)){
+
+  # if(str_starts(as.character(df_network[i,1]), "0")){
+  #   x<-sub('.', '', df_network[i,1])
+  #   
+  # } else{
+    x<-as.character(df_network[i,1])
+    
+  # }
+  # if(str_starts(as.character(df_network[i,2]), "0")){
+  #  y<-sub('.', '', df_network[i,2])
+    
+  #} else{
+    y<-as.character(df_network[i,2])
+  #}
   
-  if(str_starts(as.character(df_network[i,1]), "0")){
-    x<-print(sub('.', '', df_network[i,1]))
-    
-  }
-  if(str_starts(as.character(df_network[i,2]), "0")){
-    y<-print(sub('.', '', df_network[i,2]))
-    
-  }
-  x<-str_replace("06111", "0", "")
+  # print("New pair")
+  # print(x)
+  # print(y)
+  # print(paste("Row", i))
+  # 
   
   df_network$distance[i]<-manhattan_distances[x,y]
 }
 
-# Create network
+
+# Now that we have the distance, let's define the network again
 network <- graph_from_data_frame(d=df_network, directed=T) 
-
-
 
 # Average Degree
 degree_nodes<-degree(network, mode="all", loops = F, normalized = F)
 mean(degree_nodes)
 
-# Betweenes
+# Edge Betweeness
 edge_betweenness(network, weights= E(network)$distance)
+
+# Node Betweeness
+betw<-betweenness(network, weights= E(network)$distance)
+
+# Mean betweeness
+mean(betw)
+
